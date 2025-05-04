5314312849082bd7e2e1f544b215b6004f88e07b
Jelle Meeus <sogladev@gmail.com>
2025-05-04 05:24:53 +0200

    style: apply .clang-format

--- plugin.c
+++ plugin.c
@@ -12,18 +12,20 @@ mumble_plugin_id_t ownID;
 mumble_error_t mumble_init(mumble_plugin_id_t pluginID) {
 	ownID = pluginID;
 
-	if (mumbleAPI.log(ownID, "Wow335 Positional Audio loaded") != MUMBLE_STATUS_OK) {
-		// Logging failed -> usually you'd probably want to log things like this in your plugin's
-		// logging system (if there is any)
+	if (mumbleAPI.log(ownID, "Wow335 Positional Audio loaded")
+		!= MUMBLE_STATUS_OK) {
+		// Logging failed -> usually you'd probably want to log things like this
+		// in your plugin's logging system (if there is any)
 	}
 
 	return MUMBLE_STATUS_OK;
 }
 
 void mumble_shutdown() {
-	if (mumbleAPI.log(ownID, "Wow335 Positional Audio unloaded") != MUMBLE_STATUS_OK) {
-		// Logging failed -> usually you'd probably want to log things like this in your plugin's
-		// logging system (if there is any)
+	if (mumbleAPI.log(ownID, "Wow335 Positional Audio unloaded")
+		!= MUMBLE_STATUS_OK) {
+		// Logging failed -> usually you'd probably want to log things like this
+		// in your plugin's logging system (if there is any)
 	}
 }
 
@@ -31,36 +33,41 @@ struct MumbleStringWrapper mumble_getName() {
 	static const char *name = "Wow335 Positional Audio";
 
 	struct MumbleStringWrapper wrapper;
-	wrapper.data = name;
-	wrapper.size = strlen(name);
+	wrapper.data           = name;
+	wrapper.size           = strlen(name);
 	wrapper.needsReleasing = false;
 
 	return wrapper;
 }
 
 mumble_version_t mumble_getAPIVersion() {
-	// This constant will always hold the API version  that fits the included header files
+	// This constant will always hold the API version  that fits the included
+	// header files
 	return MUMBLE_PLUGIN_API_VERSION;
 }
 
 void mumble_registerAPIFunctions(void *apiStruct) {
-	// Provided mumble_getAPIVersion returns MUMBLE_PLUGIN_API_VERSION, this cast will make sure
-	// that the passed pointer will be cast to the proper type
+	// Provided mumble_getAPIVersion returns MUMBLE_PLUGIN_API_VERSION, this
+	// cast will make sure that the passed pointer will be cast to the proper
+	// type
 	mumbleAPI = MUMBLE_API_CAST(apiStruct);
 }
 
 void mumble_releaseResource(const void *pointer) {
-	// As we never pass a resource to Mumble that needs releasing, this function should never
-	// get called
-	printf("Called mumble_releaseResource but expected that this never gets called -> Aborting");
+	// As we never pass a resource to Mumble that needs releasing, this function
+	// should never get called
+	printf("Called mumble_releaseResource but expected that this never gets "
+		   "called -> Aborting");
 	abort();
 }
 
 
-// Below functions are not strictly necessary but every halfway serious plugin should implement them nonetheless
+// Below functions are not strictly necessary but every halfway serious plugin
+// should implement them nonetheless
 
 mumble_version_t mumble_getVersion() {
-	// below should match the description in the manifest.xml and CmakeLists.txt file
+	// below should match the description in the manifest.xml and CmakeLists.txt
+	// file
 	mumble_version_t version;
 	version.major = 0;
 	version.minor = 1;
@@ -73,21 +80,25 @@ struct MumbleStringWrapper mumble_getAuthor() {
 	static const char *author = "sogladev";
 
 	struct MumbleStringWrapper wrapper;
-	wrapper.data = author;
-	wrapper.size = strlen(author);
+	wrapper.data           = author;
+	wrapper.size           = strlen(author);
 	wrapper.needsReleasing = false;
 
 	return wrapper;
 }
 
 struct MumbleStringWrapper mumble_getDescription() {
-	// below should match the description in the manifest.xml and CmakeLists.txt file
-	static const char *description = "A positional audio plugin for World of Warcraft (x86) version 3.3.5a.12340";
+	// below should match the description in the manifest.xml and CmakeLists.txt
+	// file
+	static const char *description = "A positional audio plugin for World of "
+									 "Warcraft (x86) version 3.3.5a.12340";
 
 	struct MumbleStringWrapper wrapper;
-	wrapper.data = description;
-	wrapper.size = strlen(description);
+	wrapper.data           = description;
+	wrapper.size           = strlen(description);
 	wrapper.needsReleasing = false;
 
 	return wrapper;
 }
+
+// Positional audio
